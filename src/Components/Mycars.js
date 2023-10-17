cfc63121cff4add9b5a6bcb53c8b959b5332e7a7
Amine1342 <benabdelkader.amine1@gmail.com>
2023-10-17 17:14:03 +0100

    Alerte noCopy

--- src/Components/Mycars.js
+++ src/Components/Mycars.js
@@ -2,11 +2,16 @@ import React from "react";
 import Car from "./Cars";
 
 class MyCars extends React.Component {
-    render() {
 
+    noCopy = () => {
+        alert("Voler c'est mal !");
+    }
+
+    render() {
         return (
             <div>
                 <h1>{this.props.title}</h1>
+                <p onCopy={this.noCopy}>Un texte que je ne devrais pas pouvoir copier</p>
                 <Car color="blue">Peugeot</Car>
                 <Car color="">Toyota</Car>
                 <Car color="green"></Car>
